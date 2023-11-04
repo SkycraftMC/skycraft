648d7fd8360a4033b161f3fc18333fe8ecbddcf5
SkyfallWasTaken <mahadkalam1234@gmail.com>
2023-11-04 11:53:38 +0000

    Add some test arguments

--- src/cheerpj.ts
+++ src/cheerpj.ts
@@ -1,10 +1,16 @@
+import getLibraries from "./libraries";
+
 export async function initCheerpj() {
   await cheerpjInit({
     javaProperties: ["java.library.path=natives"],
   });
+  console.info("Java classpath: " + getLibraries().join(":"));
   cheerpjCreateDisplay(-1, -1, document.getElementById("container"));
   const exitCode = await cheerpjRunMain(
     "net.minecraft.client.main.Main",
-    "/app/mc/libraries/com/mojang/minecraft/1.12.2/minecraft-1.12.2-client.jar:/app/mc/libraries/net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar"
+    getLibraries().join(":"),
+    "--demo",
+    "boing",
+    "1.12.2"
   );
 }
