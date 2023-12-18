84b6271c386602098d9da32fa765ae4f9e484678
firecoder1 <github@skyfall.33mail.com>
2023-12-18 22:37:35 +0000

    LWJGL + freindly log

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import "./style.css";
 
 import { initCheerpj } from "./cheerpj";
 
-await initCheerpj();
+// TODO: get rid of the IIFE
+(async () => {
+	console.log(
+		"%cHi! Welcome to Skycraft!",
+		"font-size: 2em; color: #00ff00;",
+	);
+	console.log(
+		"%cHere to debug? Make sure all logging levels are enabled in your DevTools console (including Verbose)",
+		"font-size: 1.5em;",
+	);
+	await initCheerpj();
+})();
