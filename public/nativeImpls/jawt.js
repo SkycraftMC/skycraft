84b62716f6e895e5ba47ac0c39061ff14ac0d547
firecoder1 <github@skyfall.33mail.com>
2023-12-22 21:46:53 +0000

    configs and stuff

--- public/nativeImpls/jawt.js
+++
@@ -1,5 +0,0 @@
-export async function Java_TestClass_nativeAlert(lib, str) {
-	window.alert(str);
-}
-
-export default { Java_TestClass_nativeAlert };
---
+++ src/nativeImpls/lwjgl/main.ts
@@ -0,0 +1,3 @@
+import { Java_org_lwjgl_DefaultSysImplementation_getPointerSize } from "lwjgl-natives-webgl";
+
+export { Java_org_lwjgl_DefaultSysImplementation_getPointerSize };
