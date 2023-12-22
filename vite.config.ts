b7cd158763af88941b66ba5090d9b9f5580a10a7
firecoder1 <github@skyfall.33mail.com>
2023-12-22 22:10:15 +0000

    add @nabla/vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait()],
+	plugins: [wasm(), eslintPlugin(), topLevelAwait()],
 	build: {
 		outDir: "dist", // Change this to your desired output directory
 		minify: true,
